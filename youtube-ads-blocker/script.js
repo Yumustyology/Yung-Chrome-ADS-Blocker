dff105cee1a4e5153d0f124f56099f77da837975
Yusuf Mustahan <yumustyology@gmail.com>
2023-12-08 18:34:51 +0100

    feat:chrome-ads-blocker-flavours

--- youtube-ads-blocker/script.js
+++ youtube-ads-blocker/script.js
@@ -1,7 +1,33 @@
-setInterval(()=>{
-let skipButton = document.getElementsByClassName('ytp-ad-skip-button')
-if(skipButton != undefined && skipButton.length > 0){
-    console.log('Ad detected')
-    skipButton[0].click()
-}
-},3000)
+let firstInterval = setInterval(() => {
+  let skipButton1 = document.getElementsByClassName("ytp-ad-skip-button");
+  let skipButton2 = document.getElementsByClassName(
+    "ytp-ad-skip-button-modern ytp-button"
+  );
+
+  if (skipButton1 !== undefined && skipButton1.length > 0) {
+    console.log("Ad detected for 1");
+    skipButton1[0].click();
+  } else if (skipButton2 !== undefined && skipButton2.length > 0) {
+    console.log("Ad detected for 2");
+    skipButton2[0].click();
+  }
+}, 100);
+
+setTimeout(() => {
+  clearInterval(firstInterval);
+}, 2000);
+
+setInterval(() => {
+  let skipButton1 = document.getElementsByClassName("ytp-ad-skip-button");
+  let skipButton2 = document.getElementsByClassName(
+    "ytp-ad-skip-button-modern ytp-button"
+  );
+
+  if (skipButton1 !== undefined && skipButton1.length > 0) {
+    console.log("Ad detected for 1");
+    skipButton1[0].click();
+  } else if (skipButton2 !== undefined && skipButton2.length > 0) {
+    console.log("Ad detected for 2");
+    skipButton2[0].click();
+  }
+}, 3000);
