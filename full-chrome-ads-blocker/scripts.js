d51f630254c5060784f1f97b848440de0d949ef1
Yusuf Mustahan <yumustyology@gmail.com>
2023-12-08 18:14:15 +0100

    feat:chrome-ads-blocker-flavours

---
+++ full-chrome-ads-blocker/scripts.js
@@ -0,0 +1,33 @@
+let firstInterval = setInterval(() => {
+  let skipButton1 = document.getElementsByClassName("ytp-ad-skip-button");
+  let skipButton2 = document.getElementsByClassName(
+    "ytp-ad-skip-button-modern ytp-button"
+  );
+
+  if (skipButton1 !== undefined && skipButton1.length > 0) {
+    console.log("Ad detected for 1");
+    skipButton1[0].click();
+  } else if (skipButton2 !== undefined && skipButton2.length > 0) {
+    console.log("Ad detected for 2");
+    skipButton2[0].click();
+  }
+}, 100);
+
+setTimeout(() => {
+  clearInterval(firstInterval);
+}, 2000);
+
+setInterval(() => {
+  let skipButton1 = document.getElementsByClassName("ytp-ad-skip-button");
+  let skipButton2 = document.getElementsByClassName(
+    "ytp-ad-skip-button-modern ytp-button"
+  );
+
+  if (skipButton1 !== undefined && skipButton1.length > 0) {
+    console.log("Ad detected for 1");
+    skipButton1[0].click();
+  } else if (skipButton2 !== undefined && skipButton2.length > 0) {
+    console.log("Ad detected for 2");
+    skipButton2[0].click();
+  }
+}, 3000);
